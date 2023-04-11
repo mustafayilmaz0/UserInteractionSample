4bd82c4eb006a4195fa9711e8e5ba22aace2fe3c
mustafayilmaz <mstfylmaz0@gmail.com>
2023-04-11 14:04:15 +0300

    Spinner view added

--- app/src/main/java/com/mustafayilmaz/userinteractionsample/MainActivity.kt
+++ app/src/main/java/com/mustafayilmaz/userinteractionsample/MainActivity.kt
@@ -4,12 +4,10 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.PopupMenu
 import com.google.android.material.snackbar.Snackbar
 import com.mustafayilmaz.userinteractionsample.databinding.ActivityMainBinding
 
@@ -21,6 +19,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//Todo: Spinner
+        val languages = resources.getStringArray(R.array.languages)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        binding.spinner.adapter = adapter
+
+        binding.spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long){
+
+                Toast.makeText(applicationContext,getString(R.string.selected_item) + " " +"" + languages[position],Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
 
 // TODO: Toast
         binding.btnToast.setOnClickListener {
